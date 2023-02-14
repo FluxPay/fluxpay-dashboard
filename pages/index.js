d1824bf9378538f8c963e36fdbcbf5cecc1fa575
Ramanjs <raman.s2662@gmail.com>
2023-02-14 19:44:35 +0530

    fix: title

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>FLuxPay</title>
+        <title>FluxPay</title>
         <meta name="description" content="Simplify your DAO payroll management with Fluxpay" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
