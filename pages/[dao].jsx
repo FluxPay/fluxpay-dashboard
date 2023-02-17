3cb670c80b6d4c4253ff75dd83189c740133a286
Ramanjs <raman.s2662@gmail.com>
2023-02-18 02:35:43 +0530

    update: admin page logic

--- pages/[dao].jsx
+++ pages/[dao].jsx
@@ -46,11 +46,28 @@ export default function Dao() {
   return (
     <section>
       {curDao && (
-        <div className="w-full flex justify-between items-center my-24 px-8 border-2 rounded-md bg-white shadow-md">
-          <div className="w-[25%]">
-            <img className="w-full h-full object-cover" src={curDao.image} alt="dao"/>
-          </div>
+        <div className="w-full max-w-screen-lg  my-24 mx-auto px-8 border-2 rounded-md bg-white shadow-md">
+          <div className="w-full flex justify-between items-center">
+            <div className="w-[25%] ">
+              <img className="w-full h-full object-cover" src={curDao.image} alt="dao"/>
+            </div>
           <p className="w-[70%] text-lg">{curDao.description}</p>
+          </div>
+          {isAdmin ? (
+              <div className="w-full flex flex-col my-8 space-y-4">
+                <h1 className="text-fgreen text-2xl font-bold">Start Payroll</h1>
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="flow">Flow Rate</label>
+                  <input className="border-2 border-gray-200 p-2" id="flow" type="text" placeholder="Flow Rate/second"/>
+                </div>
+                <div className="flex flex-col space-y-2">
+                  <label htmlFor="nft">NFT Address</label>
+                  <input className="border-2 border-gray-200 p-2" id="nft" type="text" placeholder="NFT Collection Address"/>
+                </div>
+                <button className="btn">Create</button>
+              </div>
+            ) : null
+          }
         </div>
       )}
     </section>
