08e2795844e1771a12eb5d5678bc2b733b3a1a26
naman-ng <naman21171@iiitd.ac.in>
2023-02-18 02:00:31 +0530

    Add: Pool address and abi

--- Addresses/index.js
+++ Addresses/index.js
@@ -1,3 +1,5 @@
 export const SuperTokenFactoryAddr = '0x200657E2f123761662567A1744f9ACAe50dF47E6'; 
 export const FXP_token_address = '0xa177753Ad7b2847142631e76C65888c5a1390D17';
 export const fluxpay_address = '0xCb6A50AD10FD687745c15C9eEf972E64DC225D2F';
+export const PoolMaster_address = '0xfe5E4EEC148f1EE4172a87cfB89EADCcC79929ca';
+export const Pool_address = '0xeF726f942F58D3ca2DCe0E0D076fdB9F4C05CD80';
