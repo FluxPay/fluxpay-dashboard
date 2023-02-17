5cf07ecc7651df90dd42d79f5aab365cd712f446
Ramanjs <raman.s2662@gmail.com>
2023-02-17 19:35:45 +0530

    update: make navbar fixed

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,10 +1,9 @@
 import { ConnectButton } from '@rainbow-me/rainbowkit';
 import Link from 'next/link';
-import * as React from 'react';
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row w-screen h-fit py-4 px-[10%] justify-between items-center text-primary">
+    <div className="fixed top-0 flex flex-row w-screen h-fit py-4 px-[10%] justify-between items-center text-primary">
       <Link href="/" className="text-[1.5vmax] font-bold">
         FluxPay
       </Link>
